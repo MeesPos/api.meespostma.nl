11aecbd2f434e9a426098046b1d12c53112e77e4
MeesPos <meespostma_@hotmail.com>
2022-09-22 15:38:58 +0200

    Added Cors

--- api.meespostma.nl/Program.cs
+++ api.meespostma.nl/Program.cs
@@ -20,6 +20,13 @@ builder.Host.UseSerilog((ctx, lc) =>
     lc.WriteTo.Console().ReadFrom.Configuration(ctx.Configuration)
 );
 
+builder.Services.AddCors(options => {
+    options.AddPolicy("AllowAll",
+        b => b.AllowAnyMethod()
+               .AllowAnyHeader()
+               .AllowAnyOrigin());
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -30,6 +37,8 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("AllowAll");
 
 app.UseAuthorization();
 
