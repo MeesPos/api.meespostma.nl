76d5bbbbf07fa618caf78ea501be9e90388bf2ad
MeesPos <meespostma_@hotmail.com>
2022-09-23 10:08:08 +0200

    Added ProjectCreateDto

--- api.meespostma.nl/Configurations/MapperConfig.cs
+++ api.meespostma.nl/Configurations/MapperConfig.cs
@@ -1,7 +1,11 @@
-﻿namespace api.meespostma.nl.Configurations
-{
-    public class MapperConfig
-    {
+﻿using AutoMapper;
+using api.meespostma.nl.Data;
+using api.meespostma.nl.Models.Projects;
 
+namespace api.meespostma.nl.Configurations
+{
+    public class MapperConfig : Profile
+    {
+        CreateMap<ProjectCreateDto, Project>().ReverseMap();
     }
 }
---
+++ api.meespostma.nl/Models/Projects/ProjectCreateDto.cs
@@ -0,0 +1,15 @@
+﻿namespace api.meespostma.nl.Models.Projects
+{
+    public class ProjectCreateDto
+    {
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+        public string Url { get; set; }
+
+        public string UrlPlaceholder { get; set; }
+
+        public byte[] Logo { get; set; }
+    }
+}
