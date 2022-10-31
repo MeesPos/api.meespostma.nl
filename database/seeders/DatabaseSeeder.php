154a6f0a5696d73d6661a6757f4f10553f377cc5
MeesPos <meespostma_@hotmail.com>
2022-10-31 11:05:05 +0100

    Added user seed

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,11 +14,10 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+         \App\Models\User::factory()->create([
+             'name' => 'Mees Postma',
+             'email' => 'mail@meespostma.nl',
+             'password' => bcrypt('secret')
+         ]);
     }
 }
